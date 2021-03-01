62a72248f8d7568a30843676b177362a3372b8e7
Kulagin Yuri <jkulvichi@gmail.com>
2021-03-02 01:24:03 +0300

    added persons' search api

--- Actions/ActionPersonsSearch.php
+++ Actions/ActionPersonsSearch.php
@@ -2,6 +2,9 @@
 
 namespace Actions {
 
+    use ErrorException;
+    use Models\PersonPriz01;
+
     /**
      * Class ActionPersonsSearch to search persons
      * @package Actions
@@ -16,12 +19,48 @@ namespace Actions {
         {
             switch ($ctx->method) {
                 case "GET":
-                    (new Response("OK"))->Reply();
+                    $this->ExecuteGET($ctx);
                     break;
                 default:
                     (new Response())->AddError(405, "incorrect_method")->AddContext($ctx)->Reply();
             }
 
+        }
+
+        /**
+         * GET request actions
+         * @param Context $ctx Request context
+         * @throws ErrorException
+         */
+        private function ExecuteGET($ctx)
+        {
+            $query = $ctx->args["query"];
+
+            // exit if empty query
+            if (empty($query)) {
+                (new Response())
+                    ->AddError(404, "empty_query", "add query param to initiate search")
+                    ->AddContext($ctx)
+                    ->Reply();
+                return;
+            }
+
+            // limit of PG response models of each type
+            $pgLimit = 10;
+
+            // searching
+            $pgResFullName = (new PersonPriz01($ctx->pg))->findAllByFullName($query, "", $pgLimit);
+            $pgResBirthYear = (new PersonPriz01($ctx->pg))->findAllByBirthYear($query, "", $pgLimit);
+            $pgResPersonalID = (new PersonPriz01($ctx->pg))->findAllByPersonalID($query, "", $pgLimit);
+
+            // make response model
+            $resp = [
+                "fullName" => $pgResFullName,
+                "birthYear" => $pgResBirthYear,
+                "personalID" => $pgResPersonalID,
+            ];
+
+            (new Response($resp))->Reply();
         }
     }
 
